7034ccb7d816d4d57e004563fd93700de5dbe5cb
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-12-30 19:14:49 -0800

    Initial Commit

---
+++ Weather Timeline/Weather_TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TimelineApp.swift
+//  Weather Timeline
+//
+//  Created by Eliot Hertenstein on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
